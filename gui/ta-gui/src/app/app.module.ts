d3feabbcea4458f8f24fce17c5b425a2d920ad91
pedrophls <phls73376@gmail.com>
2018-04-27 17:12:37 -0300

    running tests

--- gui/ta-gui/src/app/app.module.ts
+++ gui/ta-gui/src/app/app.module.ts
@@ -1,36 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunoService } from './alunos/shared/aluno.service';
 import { AppComponent } from './app.component';
-import { MetasComponent } from './metas.component';
-import { AlunosComponent } from './alunos.component';
-import { AlunoService } from './aluno.service';
+import { AutoAvaliacaoComponent } from './autoAvaliacao/autoAvaliacao.component';
+import { MetasComponent } from './metas/metas.component';
+import { DiscrepanciasComponent } from './discrepancias/discrepancias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MetasComponent,
-    AlunosComponent
+    AlunosComponent,
+    AutoAvaliacaoComponent,
+    DiscrepanciasComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule, 
+    HttpModule,
     RouterModule.forRoot([
       {
-        path: 'metas',
-        component: MetasComponent
+        path: 'alunos',
+        component: AlunosComponent,
       },
       {
-        path: 'alunos',
-        component: AlunosComponent
-      }
-    ])
+        path: 'metas',
+        component: MetasComponent,
+      },
+      {
+        path: 'autoAvaliacao',
+        component: AutoAvaliacaoComponent,
+      },
+      {
+        path: 'discrepancias',
+        component: DiscrepanciasComponent,
+      },
+    ]),
   ],
   providers: [AlunoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
